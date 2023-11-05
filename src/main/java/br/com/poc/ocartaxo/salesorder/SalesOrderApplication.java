7c462f80eb705a3ff6d2e9561a3b405a30844aa2
ocartaxo <oacartax@gmail.com>
2023-11-05 15:21:47 -0300

    init

---
+++ src/main/java/br/com/poc/ocartaxo/salesorder/SalesOrderApplication.java
@@ -0,0 +1,13 @@
+package br.com.poc.ocartaxo.salesorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesOrderApplication.class, args);
+    }
+
+}
