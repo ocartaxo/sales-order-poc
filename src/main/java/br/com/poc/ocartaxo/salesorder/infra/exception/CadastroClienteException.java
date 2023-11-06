fe89c277a272efe1f15502f81689deb6aab88d60
ocartaxo <oacartax@gmail.com>
2023-11-05 21:42:00 -0300

    refactor: herda de RuntimeException

--- src/main/java/br/com/poc/ocartaxo/salesorder/infra/exception/CadastroClienteException.java
+++ src/main/java/br/com/poc/ocartaxo/salesorder/infra/exception/CadastroClienteException.java
@@ -1,6 +1,6 @@
 package br.com.poc.ocartaxo.salesorder.infra.exception;
 
-public class CadastroClienteException extends IllegalArgumentException {
+public class CadastroClienteException extends RuntimeException {
     public CadastroClienteException(String message) {
         super(message);
     }
