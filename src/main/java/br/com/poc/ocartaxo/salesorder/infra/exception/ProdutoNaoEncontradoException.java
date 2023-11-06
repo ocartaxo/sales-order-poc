609908c027d77e25b12730de73f9f90f2449cc73
ocartaxo <oacartax@gmail.com>
2023-11-05 22:52:47 -0300

    exceção para produto não encontrado

---
+++ src/main/java/br/com/poc/ocartaxo/salesorder/infra/exception/ProdutoNaoEncontradoException.java
@@ -0,0 +1,7 @@
+package br.com.poc.ocartaxo.salesorder.infra.exception;
+
+public class ProdutoNaoEncontradoException extends RuntimeException {
+    public ProdutoNaoEncontradoException(String message) {
+        super(message);
+    }
+}
