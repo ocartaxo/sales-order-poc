500b255b5c10f6fc5c38a69e3f70ca723d940da9
ocartaxo <oacartax@gmail.com>
2023-11-05 22:45:00 -0300

    mapeia dto e entidade produto

---
+++ src/main/java/br/com/poc/ocartaxo/salesorder/mapper/ProdutoMapper.java
@@ -0,0 +1,11 @@
+package br.com.poc.ocartaxo.salesorder.mapper;
+
+import br.com.poc.ocartaxo.salesorder.dto.ProdutoCadastroRequest;
+import br.com.poc.ocartaxo.salesorder.dto.ProdutoResponse;
+import br.com.poc.ocartaxo.salesorder.model.Produto;
+
+public interface ProdutoMapper {
+
+    Produto converteParaEntidade(ProdutoCadastroRequest body);
+    ProdutoResponse converteParaDto(Produto produto);
+}
