e5bab6c7d9b9dcfb0a0a2f5adf220550b471230c
ocartaxo <oacartax@gmail.com>
2023-11-08 08:19:43 -0300

    gerencia erro de produto nao encontrado

--- src/main/java/br/com/poc/ocartaxo/salesorder/infra/api/ErrorHandler.java
+++ src/main/java/br/com/poc/ocartaxo/salesorder/infra/api/ErrorHandler.java
@@ -3,6 +3,7 @@ package br.com.poc.ocartaxo.salesorder.infra.api;
 import br.com.poc.ocartaxo.salesorder.infra.exception.CadastroClienteException;
 import br.com.poc.ocartaxo.salesorder.infra.exception.CadastroProdutoException;
 import br.com.poc.ocartaxo.salesorder.infra.exception.ClienteNaoEncontradoException;
+import br.com.poc.ocartaxo.salesorder.infra.exception.ProdutoNaoEncontradoException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -50,7 +51,7 @@ public class ErrorHandler {
     }
 
     @ExceptionHandler(CadastroProdutoException.class)
-    public ResponseEntity<ApiErrorResponse> gerenciaErroCadastroProduto(
+    public ResponseEntity<ApiErrorResponse> gerenciaErroCadastroProdutoError(
             CadastroProdutoException ex,
             HttpServletRequest request
     ){
@@ -59,6 +60,20 @@ public class ErrorHandler {
                 HttpStatus.BAD_REQUEST.value(),
                 request.getRequestURI(),
                 "Ocorreu um erro ao cadastrar um novo produto",
+                ex.getMessage()
+        ));
+    }
+
+    @ExceptionHandler(ProdutoNaoEncontradoException.class)
+    public ResponseEntity<ApiErrorResponse> gerenciaProdutoNaoEncontradoError(
+            ProdutoNaoEncontradoException ex,
+            HttpServletRequest request
+    ) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ApiErrorResponse(
+                LocalDateTime.now().format(df),
+                HttpStatus.NOT_FOUND.value(),
+                request.getRequestURI(),
+                "Ocorreu um erro ao buscar pelo produto.",
                 ex.getMessage()
         ));
     }
