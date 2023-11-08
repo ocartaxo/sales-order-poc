ea74c71106155a370c12a33a59307972d279da09
ocartaxo <oacartax@gmail.com>
2023-11-08 08:57:15 -0300

    refactor: categorias do produto

--- src/main/java/br/com/poc/ocartaxo/salesorder/enums/CategoriaProduto.java
+++ src/main/java/br/com/poc/ocartaxo/salesorder/enums/CategoriaProduto.java
@@ -1,9 +1,11 @@
 package br.com.poc.ocartaxo.salesorder.enums;
 
 public enum CategoriaProduto {
+    ARTESANATO,
     CELULAR,
     VIDEOGAME,
     INFORMATICA,
     ELETRODOMESTICO,
-    LIVRO
+    LIVRO,
+    PAPELARIA
 }
