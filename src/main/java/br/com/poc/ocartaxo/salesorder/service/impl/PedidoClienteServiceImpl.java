2f3f22b5ac9d33a8c16a589346e3e07972b36a06
ocartaxo <oacartax@gmail.com>
2023-11-08 14:15:33 -0300

    fix: comparação do tipoEndereco no filter

--- src/main/java/br/com/poc/ocartaxo/salesorder/service/impl/PedidoClienteServiceImpl.java
+++ src/main/java/br/com/poc/ocartaxo/salesorder/service/impl/PedidoClienteServiceImpl.java
@@ -7,8 +7,10 @@ import br.com.poc.ocartaxo.salesorder.model.Cliente;
 import br.com.poc.ocartaxo.salesorder.repository.ClientesRepository;
 import br.com.poc.ocartaxo.salesorder.service.PedidoClienteService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PedidoClienteServiceImpl implements PedidoClienteService {
@@ -28,18 +30,20 @@ public class PedidoClienteServiceImpl implements PedidoClienteService {
 
     @Override
     public EnderecosPedido getEnderecoEntregaCobranca(Cliente cliente) {
-        final var enderecos = cliente.getEnderecos();
 
-        final var enderecoAll = enderecos.stream().filter(e-> e.getTipoEndereco().equals(TipoEndereco.ALL)).findFirst();
+        final var enderecos = cliente.getEnderecos();
+        log.info("Enderecos={}", enderecos);
+
+        final var enderecoAll = enderecos.stream().filter(e-> e.getTipoEndereco() == TipoEndereco.ALL).findFirst();
         if (enderecoAll.isPresent()){
             return new EnderecosPedido(enderecoAll.get().getLogradouro(), enderecoAll.get().getLogradouro());
         }
 
-        final var enderecoCobranca = enderecos.stream().filter( e -> e.getTipoEndereco().equals(TipoEndereco.COBRANCA)).findFirst();
-        final var enderecoEntrega = enderecos.stream().filter(e -> e.getTipoEndereco().equals(TipoEndereco.ENTREGA)).findFirst();
+        final var enderecoCobranca = enderecos.stream().filter( e -> e.getTipoEndereco() == TipoEndereco.COBRANCA).findFirst();
+        final var enderecoEntrega = enderecos.stream().filter(e -> TipoEndereco.ENTREGA.equals(e.getTipoEndereco())).findFirst();
 
         if (enderecoEntrega.isEmpty() || enderecoCobranca.isEmpty()){
-            throw new CadastroPedidoException("Error ao definir o endereco de entrega e cobranca do pedido!");
+            throw new CadastroPedidoException("Não foi possível definir os enderecos de entrega e cobranca do pedido! do cliente %s".formatted(cliente.getNome()));
         }
 
         return new EnderecosPedido(enderecoEntrega.get().getLogradouro(), enderecoCobranca.get().getLogradouro());
