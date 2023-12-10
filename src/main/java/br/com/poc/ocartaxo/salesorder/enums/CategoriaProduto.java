53a661965d37855997e57ee7a83d147d22217ac4
ocartaxo <oacartax@gmail.com>
2023-12-10 20:53:12 -0300

    categorias

--- src/main/java/br/com/poc/ocartaxo/salesorder/enums/CategoriaProduto.java
+++ src/main/java/br/com/poc/ocartaxo/salesorder/enums/CategoriaProduto.java
@@ -1,11 +1,14 @@
 package br.com.poc.ocartaxo.salesorder.enums;
 
 public enum CategoriaProduto {
+    AUTOMOTIVO,
     ARTESANATO,
     CELULAR,
+    CAMA,
     VIDEOGAME,
     INFORMATICA,
     ELETRODOMESTICO,
     LIVRO,
-    PAPELARIA
+    PAPELARIA,
+    TV
 }
