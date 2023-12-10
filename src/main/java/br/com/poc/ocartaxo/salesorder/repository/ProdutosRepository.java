e33ea59df165544a6b9c3030b879f8858c7ffa38
ocartaxo <oacartax@gmail.com>
2023-12-10 18:08:14 -0300

    lista protudos em estoque

--- src/main/java/br/com/poc/ocartaxo/salesorder/repository/ProdutosRepository.java
+++ src/main/java/br/com/poc/ocartaxo/salesorder/repository/ProdutosRepository.java
@@ -1,7 +1,12 @@
 package br.com.poc.ocartaxo.salesorder.repository;
 
 import br.com.poc.ocartaxo.salesorder.model.Produto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProdutosRepository extends JpaRepository<Produto, Long> {
+    @Query("SELECT p FROM Produto p WHERE p.quantidadeEstoque > 0")
+    Page<Produto> buscaTodosProdutosEmEstoque(Pageable p);
 }
