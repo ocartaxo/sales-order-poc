9c3988e0bef47fa53e1476cb831050e324161345
ocartaxo <oacartax@gmail.com>
2023-12-10 19:06:45 -0300

    log

--- src/main/java/br/com/poc/ocartaxo/salesorder/validacao/impl/ValidadorProdutoImpl.java
+++ src/main/java/br/com/poc/ocartaxo/salesorder/validacao/impl/ValidadorProdutoImpl.java
@@ -5,15 +5,17 @@ import br.com.poc.ocartaxo.salesorder.dto.ProdutoCadastroRequest;
 import br.com.poc.ocartaxo.salesorder.infra.exception.AtualizacaoProdutoException;
 import br.com.poc.ocartaxo.salesorder.infra.exception.CadastroProdutoException;
 import br.com.poc.ocartaxo.salesorder.validacao.ValidadorProduto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
+@Slf4j
 @Component
 public class ValidadorProdutoImpl implements ValidadorProduto {
     @Override
     public void validaCadastroProduto(ProdutoCadastroRequest body) {
-
+        log.info("Validando o produto para cadastro={}", body);
         if (body.descricao().isEmpty() || body.quantidadeEstoque() <= 0 || ehPrecoInvalido(body.valor())) {
             throw new CadastroProdutoException("Dados de cadastro do produto inválidos!");
         }
