8862673787b20706bdf861177718bdc1f9b0a861
ocartaxo <oacartaxo@gmail.com>
2025-01-29 16:29:37 -0300

    implementa interface de validacao do documento

--- src/main/java/br/com/poc/ocartaxo/salesorder/validacao/impl/ValidadorCNPJ.java
+++ src/main/java/br/com/poc/ocartaxo/salesorder/validacao/impl/ValidadorCNPJ.java
@@ -1,13 +1,15 @@
 package br.com.poc.ocartaxo.salesorder.validacao.impl;
 
 import br.com.poc.ocartaxo.salesorder.infra.exception.DocumentoInvalidoException;
+import br.com.poc.ocartaxo.salesorder.validacao.ValidadorDocumento;
 import org.springframework.stereotype.Component;
 
 import java.util.regex.Pattern;
 
 @Component
-public class ValidadorCNPJ extends ValidadorDocumento {
+public class ValidadorCNPJ implements ValidadorDocumento {
     private static final Pattern padrao = Pattern.compile("^\\d{2}\\.\\d{3}\\.\\d{3}/\\d{4}-\\d{2}$");
+
     @Override
     public void verificaDocumento(String cnpj) {
         if(!(padrao.matcher(cnpj).matches())){
--- src/main/java/br/com/poc/ocartaxo/salesorder/validacao/impl/ValidadorCPF.java
+++ src/main/java/br/com/poc/ocartaxo/salesorder/validacao/impl/ValidadorCPF.java
@@ -1,12 +1,13 @@
 package br.com.poc.ocartaxo.salesorder.validacao.impl;
 
 import br.com.poc.ocartaxo.salesorder.infra.exception.DocumentoInvalidoException;
+import br.com.poc.ocartaxo.salesorder.validacao.ValidadorDocumento;
 import org.springframework.stereotype.Component;
 
 import java.util.regex.Pattern;
 
 @Component
-public class ValidadorCPF extends ValidadorDocumento {
+public class ValidadorCPF implements ValidadorDocumento {
     private static final Pattern padrao = Pattern.compile("^(?:(\\d{3}\\.){2}\\d{3}-\\d{2}|\\d{11})$");
     @Override
     public void verificaDocumento(String cpf) {
