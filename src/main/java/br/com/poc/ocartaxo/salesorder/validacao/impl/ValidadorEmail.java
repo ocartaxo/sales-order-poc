3e297c2df7963cbf98cc87fcd15fdbbce08ccb1d
ocartaxo <oacartaxo@gmail.com>
2025-01-29 16:29:07 -0300

    javadoc

--- src/main/java/br/com/poc/ocartaxo/salesorder/validacao/impl/ValidadorEmail.java
+++ src/main/java/br/com/poc/ocartaxo/salesorder/validacao/impl/ValidadorEmail.java
@@ -1,6 +1,6 @@
 package br.com.poc.ocartaxo.salesorder.validacao.impl;
 
-import br.com.poc.ocartaxo.salesorder.infra.exception.EmailInvalidoException;
+import br.com.poc.ocartaxo.salesorder.infra.exception.InformacoesClienteInvalidaException;
 import org.springframework.stereotype.Component;
 
 import java.util.regex.Pattern;
@@ -8,11 +8,16 @@ import java.util.regex.Pattern;
 @Component
 public class ValidadorEmail {
 
-    private final Pattern padrao = Pattern.compile("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
+    private final Pattern PADRAO = Pattern.compile("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$");
 
+    /**
+     * Verifica se é um e-mail válido
+     * @param email E-mail do cliente
+     * @throws InformacoesClienteInvalidaException Informa que é um e-mail inválido
+     */
     public void valida(String email) {
-        if (email.isEmpty() || !(padrao.matcher(email).find())) {
-            throw new EmailInvalidoException("Email `%s` inválido!".formatted(email));
+        if (email.isEmpty() || !(PADRAO.matcher(email).find())) {
+            throw new InformacoesClienteInvalidaException("Email `%s` inválido!".formatted(email));
         }
     }
 
