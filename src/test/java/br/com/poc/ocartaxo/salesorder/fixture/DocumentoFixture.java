d8f36bcf00967e6061115fcc213fc34ec81cda82
ocartaxo <oacartaxo@gmail.com>
2025-01-30 17:08:57 -0300

    documento e endereco fixture

---
+++ src/test/java/br/com/poc/ocartaxo/salesorder/fixture/DocumentoFixture.java
@@ -0,0 +1,10 @@
+package br.com.poc.ocartaxo.salesorder.fixture;
+
+import br.com.poc.ocartaxo.salesorder.mapper.impl.DocumentoMapper;
+
+public class DocumentoFixture {
+
+    public static DocumentoMapper mapper() {
+        return new DocumentoMapper();
+    }
+}
---
+++ src/test/java/br/com/poc/ocartaxo/salesorder/fixture/EnderecoFixture.java
@@ -0,0 +1,9 @@
+package br.com.poc.ocartaxo.salesorder.fixture;
+
+import br.com.poc.ocartaxo.salesorder.mapper.impl.EnderecoMapper;
+
+public class EnderecoFixture {
+    public static EnderecoMapper mapper() {
+        return new EnderecoMapper();
+    }
+}
